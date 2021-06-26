38a7c477a0628e489b6f561e64b23169a832f98d
Lukas <lukaschgaming@gmail.com>
2021-06-26 17:01:50 +0200

    Update 1.3.5
    Added 404 Page

--- js/stats.js
+++ js/stats.js
@@ -9,11 +9,25 @@ const getStats = async () => {
 
 
 getStats()
-  .then(repositoryCount => document.getElementById('projects').innerHTML = repositoryCount + ' <em>Projects</em>')
-  //.then(codeFrequency => document.getElementById('code_frequency').innerHTML = codeFrequency + ' <em>Code Frequency</em>')
-  //.then(allTimeCommits => document.getElementById('all_time_commits').innerHTML = allTimeCommits + ' <em>All Time Commits</em>')
-  //.then(placeholder1 => document.getElementById('placeholder_1').innerHTML = placeholder1 + ' <em>Placeholder 1</em>')
-  //.then(placeholder2 => document.getElementById('placeholder_2').innerHTML = placeholder2 + ' <em>Placeholder 2</em>')
-  //.then(placeholder3 => document.getElementById('placeholder_3').innerHTML = placeholder3 + ' <em>Placeholder 3</em>')
+  .then(repositoryCount => document.getElementById('projects')
+    .innerHTML = repositoryCount + ' <em>Projects</em>')
+
+  //.then(codeFrequency => document.getElementById('code_frequency')
+  //  .innerHTML = codeFrequency + ' <em>Code Frequency</em>')
+
+  //.then(allTimeCommits => document.getElementById('all_time_commits')
+  //  .innerHTML = allTimeCommits + ' <em>All Time Commits</em>')
+
+  //.then(placeholder1 => document.getElementById('placeholder_1')
+  //  .innerHTML = placeholder1 + ' <em>Placeholder 1</em>')
+
+  //.then(placeholder2 => document.getElementById('placeholder_2')
+  //  .innerHTML = placeholder2 + ' <em>Placeholder 2</em>')
+
+  //.then(placeholder3 => document.getElementById('placeholder_3')
+  //  .innerHTML = placeholder3 + ' <em>Placeholder 3</em>')
+
   .catch(error => console.error(error));
+
   
+
