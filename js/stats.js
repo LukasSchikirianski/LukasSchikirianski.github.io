3a21182a4487e9d08679d48112d1c9ee7949888e
Lukas <lukaschgaming@gmail.com>
2021-06-26 09:24:02 +0200

    Update 1.3.4
    GitHub repository count  is being displayed correctly now

--- js/stats.js
+++ js/stats.js
@@ -1,34 +1,19 @@
-/*
-var url = 'https://api.github.com/users/LukasSchikirianski/repos';
+const getStats = async () => {
 
-var request = new XMLHttpRequest();
-request.open('GET', url);
+  var response = await fetch('https://api.github.com/users/LukasSchikirianski/repos');
+  var data = await response.json();
+  var repositoryCount = await Object.keys(data).length;
 
-request.onload = function () {
-  // Request finished. Do processing here.
-  console.log(request.response());
-};
-
-request.send(null);
-*/
-
-window.onload = function () {
-  document.getElementById("projects").innerHTML = '1234' + ' <em>Projects</em>';
-  document.getElementById("code_frequency").innerHTML = '567' + ' <em>Code Frequency</em>';
-  document.getElementById("all_time_commits").innerHTML = '8901' + ' <em>All Time Commits</em>';
-  document.getElementById("placeholder_1").innerHTML = '12' + ' <em>Placeholder 1</em>';
-  document.getElementById("placeholder_2").innerHTML = '34' + ' <em>Placeholder 2</em>';
-  document.getElementById("placeholder_3").innerHTML = '56' + ' <em>Placeholder 3</em>';
-
-  function getRequest() {
-
-  var request = new XMLHttpRequest();
-
-  request.onload = function () {
-    document.getElementById("code_frequency").innerHTML = 'IT WORKS' + ' <em>Code Frequency</em>';
-  };
-
-  request.open("GET", "https://api.github.com/users/LukasSchikirianski/repos", true);
-  request.send();
-  }
+  return repositoryCount; //, codeFrequency, allTimeCommits, placeholder1, placeholder2, placeholder3;
 }
+
+
+getStats()
+  .then(repositoryCount => document.getElementById('projects').innerHTML = repositoryCount + ' <em>Projects</em>')
+  //.then(codeFrequency => document.getElementById('code_frequency').innerHTML = codeFrequency + ' <em>Code Frequency</em>')
+  //.then(allTimeCommits => document.getElementById('all_time_commits').innerHTML = allTimeCommits + ' <em>All Time Commits</em>')
+  //.then(placeholder1 => document.getElementById('placeholder_1').innerHTML = placeholder1 + ' <em>Placeholder 1</em>')
+  //.then(placeholder2 => document.getElementById('placeholder_2').innerHTML = placeholder2 + ' <em>Placeholder 2</em>')
+  //.then(placeholder3 => document.getElementById('placeholder_3').innerHTML = placeholder3 + ' <em>Placeholder 3</em>')
+  .catch(error => console.error(error));
+  
